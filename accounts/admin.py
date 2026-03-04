d1491a4a50f6480ad3cf8e235ac1fb201c16fbb6
rudeous13 <Vp1311Freelance@gmail.com>
2026-03-04 23:03:03 +0530

    reset

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Role, Staff, Customer
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Role)
-admin.site.register(Staff)
-admin.site.register(Customer)
