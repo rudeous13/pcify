9a286b6c3344ba77e4f1acd028c66f26e1dac4fd
rudeous13 <Vp1311Freelance@gmail.com>
2026-03-04 15:38:08 +0530

    ultimate change

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-# Default page for when first call
-def homepage(request):
-  return render(request, "Home/home.html")
--- dashboard/decorators.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-
-def admin_required(view_func):
-  return user_passes_test(
-    lambda user: user.is_authenticated and user.is_staff,
-    login_url= "/admin_login/"
-  )(view_func)
