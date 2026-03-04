f6fc3fd1eb5043754f9cdcdd3fefe0c9dd9653c8
rudeous13 <Vp1311Freelance@gmail.com>
2026-03-05 04:17:34 +0530

    template restructure

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Role, Staff, Customer
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Staff)
+admin.site.register(Customer)
